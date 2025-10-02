35b2033dedcaca6a9fa8c1a5b071cbe3b7cbf655
AV Pro <asep.cgeds@gmail.com>
2025-10-02 12:56:13 +0700

    Update menu links to point to # instead of actual URLs

--- resources/views/components/section/menu-mobile.blade.php
+++ resources/views/components/section/menu-mobile.blade.php
@@ -11,22 +11,22 @@
                 Committee <i class="fa-solid fa-angle-right"></i></a>
         </li>
         <li>
-            <a href="/faculties" wire:navigate
+            <a href="#" wire:navigate
                 class="{{ request()->is('faculties') ? 'text-green-700' : '' }} justify-between font-normal hover:text-green-700 after:content-[''] relative after:absolute flex items-center hover:after:items-center transition-all after:transition-all duration-300 after:duration-300 after:bg-green-600 hover:ps-3 after:left-0 after:h-0 hover:after:h-[5px] after:w-0 hover:after:w-[5px] after:rounded-full">Faculties
                 <i class="fa-solid fa-angle-right"></i></a>
         </li>
         <li>
-            <a href="/program-at-glance" wire:navigate
+            <a href="#" wire:navigate
                 class="{{ request()->is('program-at-glance') ? 'text-green-700' : '' }} justify-between font-normal hover:text-green-700 after:content-[''] relative after:absolute flex items-center hover:after:items-center transition-all after:transition-all duration-300 after:duration-300 after:bg-green-600 hover:ps-3 after:left-0 after:h-0 hover:after:h-[5px] after:w-0 hover:after:w-[5px] after:rounded-full">Program
                 at Glance <i class="fa-solid fa-angle-right"></i></a>
         </li>
         <li>
-            <a href="/scientific-schedule" wire:navigate
+            <a href="#" wire:navigate
                 class="{{ request()->is('scientific-schedule') ? 'text-green-700' : '' }} justify-between font-normal hover:text-green-700 after:content-[''] relative after:absolute flex items-center hover:after:items-center transition-all after:transition-all duration-300 after:duration-300 after:bg-green-600 hover:ps-3 after:left-0 after:h-0 hover:after:h-[5px] after:w-0 hover:after:w-[5px] after:rounded-full">Scientific
                 Schedule <i class="fa-solid fa-angle-right"></i></a>
         </li>
         <li>
-            <a href="/registration" wire:navigate
+            <a href="#" wire:navigate
                 class="{{ request()->is('registration') ? 'text-green-700' : '' }} justify-between font-normal hover:text-green-700 after:content-[''] relative after:absolute flex items-center hover:after:items-center transition-all after:transition-all duration-300 after:duration-300 after:bg-green-600 hover:ps-3 after:left-0 after:h-0 hover:after:h-[5px] after:w-0 hover:after:w-[5px] after:rounded-full">Registration
                 <i class="fa-solid fa-angle-right"></i></a>
         </li>
@@ -36,12 +36,12 @@
                 <i class="fa-solid fa-angle-right"></i></a>
         </li>
         <li>
-            <a href="/submission" wire:navigate
+            <a href="#" wire:navigate
                 class="{{ request()->is('submission') ? 'text-green-700' : '' }} justify-between font-normal hover:text-green-700 after:content-[''] relative after:absolute flex items-center hover:after:items-center transition-all after:transition-all duration-300 after:duration-300 after:bg-green-600 hover:ps-3 after:left-0 after:h-0 hover:after:h-[5px] after:w-0 hover:after:w-[5px] after:rounded-full">Submission
                 <i class="fa-solid fa-angle-right"></i></a>
         </li>
         <li>
-            <a href="#" wire:navigate
+            <a href="/visiting" wire:navigate
                 class="{{ request()->is('visiting') ? 'text-green-700' : '' }} justify-between font-normal hover:text-green-700 after:content-[''] relative after:absolute flex items-center hover:after:items-center transition-all after:transition-all duration-300 after:duration-300 after:bg-green-600 hover:ps-3 after:left-0 after:h-0 hover:after:h-[5px] after:w-0 hover:after:w-[5px] after:rounded-full">Visiting
                 <i class="fa-solid fa-angle-right"></i></a>
         </li>
--- resources/views/components/section/menu.blade.php
+++ resources/views/components/section/menu.blade.php
@@ -15,7 +15,7 @@
                     Committee <i class="fa-solid fa-angle-right"></i></a>
             </li>
             <li>
-                <a href="/faculties" wire:navigate
+                <a href="#" wire:navigate
                     class="{{ request()->is('faculties') ? 'text-green-400' : '' }} justify-between hover:text-green-800 ">Faculties
                     <i class="fa-solid fa-angle-right"></i></a>
             </li>
@@ -28,12 +28,12 @@
             Scientific Program <i class="fa-solid fa-angle-down"></i></div>
         <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box gap-2 w-60 p-2 shadow-sm">
             <li>
-                <a href="/program-at-glance" wire:navigate
+                <a href="#" wire:navigate
                     class="{{ request()->is('program-at-glance') ? 'text-green-400' : '' }} justify-between hover:text-green-800">Program
                     at Glance <i class="fa-solid fa-angle-right"></i></a>
             </li>
             <li>
-                <a href="/scientific-schedule" wire:navigate
+                <a href="#" wire:navigate
                     class="{{ request()->is('scientific-schedule') ? 'text-green-400' : '' }} justify-between hover:text-green-800">Scientific
                     Schedule <i class="fa-solid fa-angle-right"></i></a>
             </li>
@@ -41,7 +41,7 @@
 
 
     <li>
-        <a href="/registration" wire:navigate
+        <a href="#" wire:navigate
             class="{{ request()->is('registration') ? 'text-green-400' : 'text-green-900' }} hover:text-green-600 hover:underline">Registration
         </a>
     </li>
@@ -51,12 +51,12 @@
         </a>
     </li>
     <li>
-        <a href="/submission" wire:navigate
+        <a href="#" wire:navigate
             class="{{ request()->is('submission') ? 'text-green-400' : 'text-green-900' }} hover:text-green-600 hover:underline">Submission
         </a>
     </li>
     <li>
-        <a href="#" wire:navigate
+        <a href="/visiting" wire:navigate
             class="{{ request()->is('visiting') ? 'text-green-400' : 'text-green-900' }} hover:text-green-600 hover:underline">Visiting
         </a>
     </li>
