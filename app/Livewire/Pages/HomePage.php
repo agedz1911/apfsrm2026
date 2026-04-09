8f2f00bb2006ceb2b4dd29468810b4276dbbe52d
AV Pro <asep.cgeds@gmail.com>
2026-04-09 15:58:32 +0700

    add automatic modal dialog in homepage

--- app/Livewire/Pages/HomePage.php
+++ app/Livewire/Pages/HomePage.php
@@ -11,6 +11,23 @@ use Livewire\Component;
 #[Title('Asian Pacific Federation of Societies for Reconstructive Microsurgery')]
 class HomePage extends Component
 {
+    public $showModal = false;
+
+    public function mount()
+    {
+        // Tampilkan modal secara otomatis saat component di-mount
+        if (!session()->has('modal_shown')) {
+            $this->showModal = true;
+            session(['modal_shown' => true]);
+        }
+    }
+
+    public function closeModal()
+    {
+        $this->showModal = false;
+    }
+
+
     public function render()
     {
         $welcomeMessages = WelcomeMessage::all();
